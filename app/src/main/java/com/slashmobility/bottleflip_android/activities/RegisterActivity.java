031bb34a454378d999dbacbbe300337853d72bf0
Edgar Navarro <edgar.navarro@digital-cupcake.com>
2017-04-06 19:41:16 -0400

    Se completa maquetacion de registro

--- app/src/main/java/com/slashmobility/bottleflip_android/activities/RegisterActivity.java
+++ app/src/main/java/com/slashmobility/bottleflip_android/activities/RegisterActivity.java
@@ -1,15 +1,33 @@
 package com.slashmobility.bottleflip_android.activities;
 
-import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.slashmobility.bottleflip_android.R;
+import com.slashmobility.bottleflip_android.utils.Utils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegisterActivity extends BaseActivity {
+
+    @BindView(R.id.btn_continue)
+    Button btn_continue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        configViews();
+    }
+
+    private void configViews(){
+        changeColorBarNotification(R.color.dark_blue);
+        Utils.changeColorDrawable(btn_continue, RegisterActivity.this, R.color.white);
     }
 }
